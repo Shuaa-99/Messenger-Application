5d4bc084f12f9ecb6aa4f69af44679b77e093814
administrator <administrator@49170.local>
2021-10-27 12:36:36 +0100

    inti commit

---
+++ Messenger Application/Controllers/LoginViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Messenger Application
+//
+//  Created by administrator on 27/10/2021.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
